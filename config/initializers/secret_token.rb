be8d4383d8368f7ebd678ba71ea9b3abaa8c0c8f
Bronte Adshead <bronte.adshead@43379.auckland.natcoll.ac.nz>
2014-07-31 19:00:36 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odot::Application.config.secret_token = '7189b00db0d2378d966652140f968866436c649bc67ccfdd7d559e22a2f5ef3ef81f996b7d6dcb025035d5d4adf24c1a18f4cd842c069b0fbf91c8a4ce8fa9a6'
